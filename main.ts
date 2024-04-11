0c5eb908a9e52fab67cdd8e4d569b3fb6ac5c55e
ConvoAdmin <None>
2024-04-11 16:49:40 -0400

    Initial plugin commit

--- main.ts
+++ main.ts
@@ -1,134 +1,114 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+import { Plugin, PluginManifest, Notice, TFile } from 'obsidian';
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	constructor(app: App, manifest: PluginManifest) {
+		super(app, manifest);
+	}
 
-	async onload() {
-		await this.loadSettings();
+	onload() {
+		super.onload();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
+			id: 'replace-tasks',
+			name: 'Replace Unchecked Tasks',
+			callback: () => this.replaceTasks(),
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
+
+		this.addRibbonIcon('checkmark', 'Replace Unchecked Tasks', () => {
+			this.replaceTasks();
+			new Notice('Tasks have been replaced!');
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+
+		this.addRibbonIcon('dice', 'Roll Up Linked Tasks', () => {
+			this.rollUpTasks();
+		});
+
+
+
+	}
+
+
+	async rollUpTasks() {
+		const currentFile = this.app.workspace.getActiveFile();
+		if (!currentFile) {
+			console.log("No active file.");
+			return;
+		}
+
+		let visitedNotes = new Set();
+		let tasksByNote = new Map();
+
+		const generateBlockId = () => `id-${Math.random().toString(36).substr(2, 9)}`;
+
+		const processLinkedNotes = async (file, sourceTitle = null) => {
+			let content = await this.app.vault.read(file);
+			const wikilinkRegex = /\[\[([^\]]+)\]\]/g;
+			let match;
+
+			while ((match = wikilinkRegex.exec(content)) !== null) {
+				const linkedNoteTitle = match[1];
+				const linkedFile = this.app.vault.getAbstractFileByPath(`${linkedNoteTitle}.md`);
+
+				if (linkedFile instanceof TFile && !visitedNotes.has(linkedFile.path)) {
+					visitedNotes.add(linkedFile.path);
+
+					let linkedContent = await this.app.vault.read(linkedFile);
+					let updatedContent = linkedContent;
+					let blockId = generateBlockId();
+
+					const tasks = (linkedContent.match(/- \[ \] .+/g) || []).map(task => {
+						// Remove the "- [ ]" prefix and add a block ID for linking
+						let taskText = task.slice(6).trim();
+						updatedContent = updatedContent.replace(task, `- ${taskText} <span style="font-size: small; color: gray;">^${blockId}</span> #rolleduptask`);
+						return `- [ ] ${taskText} [[${linkedNoteTitle}#^${blockId}|↩]]`;
+					});
+
+					await this.app.vault.modify(linkedFile, updatedContent);
+
+					if (tasks.length > 0) {
+						let currentTasks = tasksByNote.get(linkedNoteTitle) || [];
+						currentTasks.push(...tasks);
+						tasksByNote.set(linkedNoteTitle, currentTasks);
 					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+					await processLinkedNotes(linkedFile, linkedNoteTitle);
 				}
 			}
-		});
+		};
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		await processLinkedNotes(currentFile);
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		if (tasksByNote.size > 0) {
+			let topLevelContent = await this.app.vault.read(currentFile);
+			tasksByNote.forEach((tasks, noteTitle) => {
+				topLevelContent += `\n\n### Rolled Up Tasks from [[${noteTitle}]]\n${tasks.join('\n')}`;
+			});
+			await this.app.vault.modify(currentFile, topLevelContent);
+		}
 	}
 
-	onunload() {
 
+
+
+
+
+
+
+
+
+
+
+	async replaceTasks() {
+		const activeLeaf = this.app.workspace.activeLeaf;
+		if (!activeLeaf) return;
+
+		const editor = activeLeaf.view.sourceMode.cmEditor;
+		const doc = editor.getDoc();
+
+		// Replace "- [ ]" with "-" and append the "#rolled-forward-task" tag
+		const newContent = doc.getValue().replace(/- \[ \] /g, '- #rolled-forward-task ');
+
+		doc.setValue(newContent);
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
 }
